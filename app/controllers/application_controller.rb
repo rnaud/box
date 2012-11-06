d5e0d9180105976db5b24ef2817b2422676a00ff
Arnaud Ferreri <ferreri@rnaud.net>
2012-11-06 15:46:33 +0100

    added password to server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "rnaud" && password == "melchi"
+    end
+  end
+
 end
