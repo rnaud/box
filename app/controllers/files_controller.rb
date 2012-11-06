a4f0c336f681578eadf527a6acfb1a03abed61ed
Arnaud Ferreri <ferreri@rnaud.net>
2012-11-06 12:54:15 +0100

    updated files

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -2,6 +2,13 @@ class FilesController < ApplicationController
 
 
   def index
+
+    if params[:dir]
+      @files = Dir.glob("public/files/#{params[:dir]}/*")
+    else
+      @files = Dir.glob("public/files/*")
+    end
+
   end
 
 end
